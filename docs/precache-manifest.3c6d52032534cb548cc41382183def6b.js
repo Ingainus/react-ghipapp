7b1a02c1ea99838f6100aef825c312ffa37811cd
Fidelmar Vaca <iscfidelmar@gmail.com>
2020-06-30 12:07:26 -0500

    docs agregado

---
+++ docs/precache-manifest.3c6d52032534cb548cc41382183def6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c5b29a23177135a76ff0e9457de6016",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b170ce7ab219f7a8b09",
+    "url": "/static/css/main.63d0de23.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b170ce7ab219f7a8b09",
+    "url": "/static/js/main.e8a839cf.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
